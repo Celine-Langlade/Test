a0c96788ef4081e1a205cf13d9e60e9cafb9a4ce
Celine-Langlade <coklicote@hotmail.fr>
2016-03-08 17:51:09 +0100

    test

---
+++ Index.php
@@ -0,0 +1,13 @@
+<?php
+
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
+
+$moyenne = (42 + 51 + 45 + 48 + 19)/5;
+
+echo "La moyenne est $moyenne";
